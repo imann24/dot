f482e502beb29cfdfb4fdc19b13e19d51cfaf9bd
Isaiah Mann <contact@isaiahmann.com>
2017-03-12 01:09:27 -0500

    Adding more code for dim

--- js/gui.js
+++ js/gui.js
@@ -8,8 +8,25 @@ class UICanvas
      constructor(paper, pen)
      {
           this.paper = paper;
+          this.originalSize = new Dimensions(paper.width, paper.height);
           this.pen = pen;
           this.elements = [];
+          this.isFullScreen = false;
+     }
+
+     toggleFullScreen()
+     {
+          if(this.isFullScreen)
+          {
+               this.paper.width = this.originalSize.width;
+               this.paper.height = this.originalSize.height;
+          }
+          else
+          {
+               this.paper.width = screen.width;
+               this.paper.height = screen.height;
+          }
+          this.isFullScreen = !this.isFullScreen;
      }
 
      addElement(elem)
@@ -19,6 +36,13 @@ class UICanvas
 
      draw()
      {
+          this.pen.clearRect
+          (
+               0,
+               0,
+               this.paper.width,
+               this.paper.height
+          );
           for(var i = 0; i < this.elements.length; i++)
           {
                this.elements[i].draw(this.pen);
@@ -26,8 +50,32 @@ class UICanvas
      }
 }
 
+class Dimensions
+{
+     constructor(width, height)
+     {
+          this.width = width;
+          this.height = height;
+     }
+}
+
+class Point
+{
+     constructor(x, y)
+     {
+          this.x = x;
+          this.y = y;
+     }
+}
+
 class UIElement
 {
+     constructor(position, dimensions)
+     {
+          this.position = position;
+          this.dimensions = dimensions;
+     }
+
      draw(pen)
      {
           // To be overriden in subclasses
--- js/main.js
+++ js/main.js
@@ -5,5 +5,19 @@
 
 function main()
 {
-     var canvas = new UICanvas();
+     var w = screen.width;
+     var h = screen.height;
+
+     var paper = document.getElementById("mainCanvas");
+     var pen = paper.getContext("2d");
+     var canvas = new UICanvas(paper, pen);
+
+     var buttonPos = new Point(w / 2, h * 3 / 4);
+     var buttonDim = new Dimensions(100, 100);
+
+     var button = new UIButton(buttonPos, buttonDim);
+
+     canvas.addElement(button);
+     canvas.toggleFullScreen();
+     canvas.draw();
 }
